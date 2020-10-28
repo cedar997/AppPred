745bd8f5c53de7ee521585743f82486cbe1f8ebd
xiexs <527474091@qq.com>
2020-10-28 16:14:05 +0800

    解决输入法时间不正确的问题

--- recorder/src/main/java/com/rom471/recorder/AccessibilityMonitorService.java
+++ recorder/src/main/java/com/rom471/recorder/AccessibilityMonitorService.java
@@ -1,6 +1,8 @@
 package com.rom471.recorder;
 
 import android.accessibilityservice.AccessibilityService;
+import android.content.ComponentName;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.view.accessibility.AccessibilityEvent;
 
@@ -16,6 +18,7 @@ public class AccessibilityMonitorService extends AccessibilityService {
     private String last ="";
     RecordDBHelper db;
     Record record;
+    PackageManager pm;
     public AccessibilityMonitorService() {
     }
 
@@ -24,6 +27,7 @@ public class AccessibilityMonitorService extends AccessibilityService {
         super.onServiceConnected();
         db=new RecordDBHelper(getApplicationContext(),"app.db");
         record=new Record();
+        pm = getPackageManager();//初始化包管理器
     }
 
     @Override
@@ -36,35 +40,42 @@ public class AccessibilityMonitorService extends AccessibilityService {
                 record(event);
                 break;
             case TYPE_VIEW_CLICKED:
-
+                record(event);
                 break;
             case TYPE_VIEW_LONG_CLICKED:
                 break;
         }
     }
-    private void record(AccessibilityEvent event){
-        mWindowClassName = event.getClassName();
+    private void saveRecord(String app_name){
+        record.setAppname(app_name);
+        DBUtils.storeBatteryInfo(getApplicationContext(),record);
+        DBUtils.storeNetworkInfo(getApplicationContext(),record);
+        db.insertRecord(record);
+    }
+    //获得应用名称
+    private String getAppLabel(AccessibilityEvent event){
         String package_str= event.getPackageName()==null?"":event.getPackageName().toString();
-        String appLable="";
-        PackageManager pm = getPackageManager();
+        String appLabel="";
         try {
-            appLable = pm.getPackageInfo(package_str,PackageManager.GET_ACTIVITIES).applicationInfo.loadLabel(pm).toString();
+            appLabel = pm.getPackageInfo(package_str,PackageManager.GET_ACTIVITIES).applicationInfo.loadLabel(pm).toString();
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
-        if(!appLable.equals("") && !last.equals(appLable)) {
-            record.setAppname(last);
-            DBUtils.storeBatteryInfo(getApplicationContext(),record);
-            DBUtils.storeNetworkInfo(getApplicationContext(),record);
-            db.insertRecord(record);
-            record.setAppname(appLable);
+        return appLabel;
 
+    }
+    private void record(AccessibilityEvent event){
+        String appLabel = getAppLabel(event);
+        if(appLabel.equals(last)){
 
-            db.insertRecord(record);
-            last =appLable;
+        }else{
+            saveRecord(last);
+            saveRecord(appLabel);
+            last=appLabel;
         }
 
     }
+    
     @Override
     public void onInterrupt() {
 
