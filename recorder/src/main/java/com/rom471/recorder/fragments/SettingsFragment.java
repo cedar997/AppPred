a753d927a44d4348408688a6c0a27c492dc6b928
wkq <wukaiqiu@mail.ustc.edu.cn>
2020-10-28 10:06:02 +0800

    实现设置界面

--- recorder/src/main/java/com/rom471/recorder/fragments/SettingsFragment.java
+++ recorder/src/main/java/com/rom471/recorder/fragments/SettingsFragment.java
@@ -1,19 +1,33 @@
 package com.rom471.recorder.fragments;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
+import com.rom471.db.AppListViewAdapter;
+import com.rom471.db.Record;
+import com.rom471.db.RecordDBHelper;
 import com.rom471.recorder.R;
+import com.rom471.recorder.RecordService;
+
+import java.util.List;
 
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener{
 
+    Button accessibility_btn;
+    RecordDBHelper db;
 
     @Nullable
     @Override
@@ -21,12 +35,30 @@ public class SettingsFragment extends Fragment {
         return inflater.inflate(R.layout.layout_settings_fragment,container,false);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
+        db=new RecordDBHelper(getContext(),"app.db");
 
 
     }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.clear_records_btn:
+                db.clearRecords();
 
+                break;
+            case R.id.open_accessibility_btn:
+                getAccessibilityPermission(getContext());
+                break;
+        }
+    }
+    public void getAccessibilityPermission(Context context){
+        final String mAction= Settings.ACTION_ACCESSIBILITY_SETTINGS;//辅助功能
+        Intent intent=new Intent(mAction);
+        context.startActivity(intent);
+    }
 }
